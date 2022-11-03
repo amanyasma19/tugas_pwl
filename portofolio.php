b5c961132e25c93ef0a2661b9d51c2e83a503242
amanyasma19 <amanyasma19@gmail.com>
2022-11-03 19:35:44 +0700

    tugas bootstrap

---
+++ portofolio.php
@@ -0,0 +1,340 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    
+    <!-- bootsrap ikon-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <!--aos-->
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+
+    <!-- my css-->
+    <link rel="stylesheet" href="style.css">
+
+  </head>
+  <body id="home">
+
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow-sm fixed-top">
+  <div class="container">
+    <a class="navbar-brand fw-bold" href="#">AMANY ASMA</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#about">about</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#pendidikan">pendidikan</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#projects">projects</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#gallery">galery</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#contact">contact</a>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+</nav> 
+  
+    <!-- akhir navbar-->
+
+
+    <!-- jumbotorn-->
+    <section class="jumbotron text-center text-danger">
+      <img src="img/profil.jpeg" alt="amany asma" width="200" class="rounded-circle img-thumbnail" >
+      <h1 class="display-4 fw-bold">AMANY ASMA!</h1>
+      <p class="lead fw-bold">Mahasiswi UNSUR</p>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="10" d="M0,160L48,170.7C96,181,192,203,288,181.3C384,160,480,96,576,64C672,32,768,32,864,80C960,128,1056,224,1152,234.7C1248,245,1344,171,1392,133.3L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+
+  
+    </section>
+
+    <!-- akhir  jumbotorn-->
+
+
+    <!-- about-->
+
+    <section id="about">
+      <div class="container" >
+        <div class="row text-center mb-4">
+          <div class="col">
+            <h2>About Me</h2>
+          </div>
+        </div>
+
+        <div class="row justify-content-center fs-5 text-center">
+          <div class="col-md-4 " data-aos="fade-right"
+          >
+            <p class="fw-bold">BIODATA SAYA</p>
+            <p>NAMA          : Amany Asma </p>
+            <p>NAMA PANGGILAN: Amany </p>
+            <p>TEMPAT LAHIR  : Bandung </p>
+            <p>TANGGAL LAHIR : 19-11-2000 </p>
+            <p>HOBBY         : Nonton film </p>
+          </div>
+          <div class="col-md-4" data-aos="fade-left"
+           data-aos-delay="200">
+            <p class="fw-bold">PENDIDIKAN SAAT INI</p>
+            <p>Mahasiwi Universitas Suryakancana</p>
+            <p>Fakultas Teknik</p>
+            <p>Program Studi Teknik Informatika</p>
+          </div>
+        </div>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#e2edff" fill-opacity="10" d="M0,32L48,74.7C96,117,192,203,288,202.7C384,203,480,117,576,74.7C672,32,768,32,864,69.3C960,107,1056,181,1152,208C1248,235,1344,213,1392,202.7L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+    </secttion>
+    <!-- akhir  about-->
+
+     <!-- pendidikan-->
+    <section id="pendidikan">
+      <div class="container">
+        <div class="row text-center mb-4">
+          <div class="col">
+            <h2>Pendidikan dan Pengalaman</h2>
+          </div>
+        </div>
+
+        <div class="row justify-content-center fs-5 text-center">
+          <div class="col-md-4 "data-aos="fade-right">
+            <p class="fw-bold">RIWAYAT PENDIDIKAN</p>
+            <p>TK         : TK DHARMA WANITA </p>
+            <p>SD         : SD NEGERI 2 JAGOAN </p>
+            <p>SMP        : SMP NEGERI 1 SAMBI </p>
+            <p>SMA        : SMA NEGERI 1 SIMO </p>
+            <p>KULIAH     : UNIVERSITAS SURYAKANCANA </p>
+          </div>
+          <div class="col-md-4"  data-aos="fade-left"
+           data-aos-delay="200">
+            <p class="fw-bold">PENGALAMAN</p>
+            <p>Anggota Osis SMP N 1 SAMBI</p>
+            <p>Anggota Paskibra SMA N 1 SIMO</p>
+            <p>Anggota paskibra SMA N 1 SIMO</p>
+          </div>
+        </div>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="10" d="M0,32L48,74.7C96,117,192,203,288,202.7C384,203,480,117,576,74.7C672,32,768,32,864,69.3C960,107,1056,181,1152,208C1248,235,1344,213,1392,202.7L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+    </secttion>
+
+
+    <!-- akhir  pendidikan-->
+
+
+    <!-- projectt-->
+    <section id="projects">
+      <div class="container">
+        <div class="row text-center mb-3">
+          <div class="col">
+            <h2>My Projects</h2>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-md-4 mb-3">
+          <div class="card" data-aos="flip-left"
+          data-aos-duration="500">
+            <img src="img/projects/1.jpg" class="card-img-top" alt="project1">
+            <div class="card-body">
+               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+          </div>
+          <div class="col-md-4 mb-3">
+          <div class="card" data-aos="flip-left"
+          data-aos-duration="500" data-aos-delay="100">
+            <img src="img/projects/2.jpg" class="card-img-top" alt="project2">
+            <div class="card-body">
+               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+          </div>
+          <div class="col-md-4 mb-3">
+          <div class="card" data-aos="flip-left"
+          data-aos-duration="500" data-aos-delay="200">
+            <img src="img/projects/3.jpg" class="card-img-top" alt="project3">
+            <div class="card-body">
+               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+          </div>
+          <div class="col-md-4 mb-3">
+          <div class="card"data-aos="flip-left"
+          data-aos-duration="500" data-aos-delay="300">
+            <img src="img/projects/4.jpg" class="card-img-top" alt="project4">
+            <div class="card-body">
+               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+          </div>
+          <div class="col-md-4 mb-3">
+          <div class="card"data-aos="flip-left"
+          data-aos-duration="500" data-aos-delay="400">
+            <img src="img/projects/5.jpg" class="card-img-top" alt="project5">
+            <div class="card-body">
+               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+          </div>
+          </div>
+        </div>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#e2edfe" fill-opacity="10" d="M0,128L48,128C96,128,192,128,288,149.3C384,171,480,213,576,208C672,203,768,149,864,149.3C960,149,1056,203,1152,202.7C1248,203,1344,149,1392,122.7L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+    </section>
+    <!-- akhir project-->
+
+
+    <!-- galery-->
+      <section id="gallery">
+        <div class="container">
+        <div class="row text-center mb-3">
+          <div class="col">
+            <h2>Galeri</h2>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-md-3"data-aos="fade-down"
+          data-aos-duration="1000" data-aos-delay="100">
+            <a href="#">
+              <img src="img/gallery/thumbnail/a.jpg" alt="gambar1" class="img-fluid galery-img"/>
+            </a>
+          </div>
+          <div class="col-md-3"data-aos="fade-down"
+          data-aos-duration="1000" data-aos-delay="100">
+            <a href="#">
+              <img src="img/gallery/thumbnail/b.jpg" alt="gambar2" class="img-fluid galery-img"/>
+            </a>
+          </div>
+          <div class="col-md-3"data-aos="fade-down"
+          data-aos-duration="1000" data-aos-delay="100">
+            <a href="#">
+              <img src="img/gallery/thumbnail/c.jpg" alt="gambar3" class="img-fluid galery-img"/>
+            </a>
+          </div>
+          <div class="col-md-3"data-aos="fade-down"
+          data-aos-duration="1000" data-aos-delay="100">
+            <a href="#">
+              <img src="img/gallery/thumbnail/d.jpg" alt="gambar4" class="img-fluid galery-img"/>
+            </a>
+          </div>
+          <div class="col-md-3"data-aos="fade-down"
+          data-aos-duration="1000" data-aos-delay="100">
+            <a href="#">
+              <img src="img/gallery/thumbnail/e.jpg" alt="gambar5" class="img-fluid galery-img"/>
+            </a>
+          </div>
+          <div class="col-md-3"data-aos="fade-down"
+          data-aos-duration="1000" data-aos-delay="100">
+            <a href="#">
+              <img src="img/gallery/thumbnail/f.jpg" alt="gambar6" class="img-fluid galery-img"/>
+            </a>
+          </div>
+          <div class="col-md-3"data-aos="fade-down"
+          data-aos-duration="1000" data-aos-delay="100">
+            <a href="#">
+              <img src="img/gallery/thumbnail/g.jpg" alt="gambar7" class="img-fluid galery-img"/>
+            </a>
+          </div>
+          <div class="col-md-3"data-aos="fade-down"
+          data-aos-duration="1000" data-aos-delay="100">
+            <a href="#">
+              <img src="img/gallery/thumbnail/h.jpg" alt="gambar8" class="img-fluid galery-img"/>
+            </a>
+          </div>
+          <div class="col-md-3"data-aos="fade-down"
+          data-aos-duration="1000" data-aos-delay="100">
+            <a href="#">
+              <img src="img/gallery/thumbnail/i.jpg" alt="gambar9" class="img-fluid galery-img"/>
+            </a>
+          </div>
+          <div class="col-md-3"data-aos="fade-down"
+          data-aos-duration="1000" data-aos-delay="100">
+            <a href="#">
+              <img src="img/gallery/thumbnail/j.jpg" alt="gambar10" class="img-fluid galery-img"/>
+            </a>
+          </div>
+        </div>
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="10" d="M0,128L48,128C96,128,192,128,288,149.3C384,171,480,213,576,208C672,203,768,149,864,149.3C960,149,1056,203,1152,202.7C1248,203,1344,149,1392,122.7L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+      </section>
+    <!-- akhir galery-->
+
+
+    <!-- contact-->
+    <section id="contact">
+      <div class="container">
+        <div class="row text-center mb-3">
+          <div class="col">
+            <h2>contact me</h2>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-md-6">
+          <form>
+              <div class="mb-3">
+                <label for="name" class="form-label">nama lengkap</label>
+                <input type="text" class="form-control" id="name" aria-describedby="name">
+              </div>
+              <div class="mb-3">
+                <label for="email" class="form-label">Email </label>
+                <input type="email" class="form-control" id="email" aria-describedby="email">
+              
+              </div>
+              <div class="mb-3">
+                <label for="pesan" class="form-label">pesan</label>
+                <textarea class="form-control" id="pesan" rows="3"></textarea>
+              </div>
+              <button type="submit" class="btn btn-primary">kirim</button>
+          </form>
+          </div>
+        </div>
+      </div>
+      
+    </section>
+    <!-- akhir contact-->
+
+    <!-- location-->
+    <section id="location">
+        <div class="container-fluid">
+          <div class="row">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3961.705013672445!2d107.13840251379173!3d-6.805693368453203!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6852e697990cef%3A0x2ddbb36bc8af404e!2sUniversitas%20Suryakancana!5e0!3m2!1sid!2sid!4v1665042245589!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+           </div>
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0d6efd" fill-opacity="10" d="M0,128L48,128C96,128,192,128,288,149.3C384,171,480,213,576,208C672,203,768,149,864,149.3C960,149,1056,203,1152,202.7C1248,203,1344,149,1392,122.7L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+    
+    </section>
+    <!-- akhir location-->
+
+
+    <!-- footer-->
+    <footer class="bg-primary text-white text-center pb-3">
+      <p>created with <i class="bi bi-suit-heart-fill text-danger"></i> by <a href="https://www.instagram.com/amany_asma/" class="text-white fw-bold"> amany asma</p>
+    </footer>
+    <!-- akhir footer-->
+
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+      <script>
+    
+    AOS.init({
+      once: true,
+      duration: 2000,
+    });
+  </script>
+ 
+
+  </body>
+</html>
